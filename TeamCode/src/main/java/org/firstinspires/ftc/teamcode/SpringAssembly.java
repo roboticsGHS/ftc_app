35e8f4cde6caf796cdffef52b57b45ae9db45b9f
roboticsGHS <mecannella@spsmail.org>
2018-05-02 07:03:29 -0500

    add other robot files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpringAssembly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpringAssembly.java
@@ -16,29 +16,10 @@ public class SpringAssembly extends OpMode {
     final static double CLAW_MIN_RANGE  = 0.20;
     final static double CLAW_MAX_RANGE  = 0.6;
 
-    // position of the arm servo.
-    double servoPosition;
-    double speed = .02;
-    double servoPosition2;
-    double servoPosition3;
-
-
-
     DcMotor leftMotor;    //drive
     DcMotor rightMotor;   //drive
-  //  DcMotor leftFront;    //frontdrive
- //   DcMotor rightFront;   //frontdrive
-//    DcMotor leftLift;
- //   DcMotor rightLift;
     DcMotor clawArm;
-//    Servo servo;  //grabbers
- //   Servo servo2; //grabbers
- //   Servo servo3; //sensor servo
-    //OpticalDistanceSensor distance;
-    //ModernRoboticsI2cGyro gyro;
-    //OpticalDistanceSensor lineFollower;
-    //TouchSensor touchSensor;
-    //ModernRoboticsI2cColorSensor colorSensor; //sensor
+
     double blue;
     /**
      * Constructor
@@ -60,43 +41,12 @@ public class SpringAssembly extends OpMode {
        * configured your robot and created the configuration file.
        */
 
-      /*  rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-*/
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
         clawArm = hardwareMap.dcMotor.get("clawArm");
-        //rightShooter = hardwareMap.dcMotor.get("rightShooter");
-        //rightShooter.setDirection(DcMotor.Direction.REVERSE);
-/*
-        leftLift = hardwareMap.dcMotor.get("leftLift");
-        rightLift = hardwareMap.dcMotor.get("rightLift");
-        rightLift.setDirection(DcMotor.Direction.REVERSE);
-*/
-        //lineFollower = hardwareMap.opticalDistanceSensor.get("lineFollower");
-        //distance = hardwareMap.opticalDistanceSensor.get("distance");
-        //colorSensor = hardwareMap.colorSensor.get("colorSensor");
-        //gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
-/*
-        servo2 = hardwareMap.servo.get("servo2");
-        servo2.setPosition(servoPosition2);
 
-        servo = hardwareMap.servo.get("servo");
-        servo.setPosition(servoPosition);
-
-        servo3 = hardwareMap.servo.get("servo3");
-        servo3.setPosition(servoPosition3);
-
-        // assign the starting position of the wrist and claw
-        servoPosition = 0.5;
-        servoPosition2 = .4;
-        //clawPosition = 0.1;
-
-        servoPosition3 = 0.5;
-*/
     }
 
     /*
@@ -113,20 +63,15 @@ public class SpringAssembly extends OpMode {
         float left_move_value = gamepad1.left_stick_y;
         float right_move_value = gamepad1.right_stick_y;
 
-        float claw_pos = gamepad2.left_stick_y;
-
-        //float lift_pos = gamepad1.left_trigger;
-        //float lift_neg = -gamepad1.right_trigger;
+        float claw_pos = gamepad1.left_trigger;
+        float claw_neg = gamepad1.right_trigger;
 
         // clip the right/left values so that the values never exceed +/- 1
         left_move_value = Range.clip(left_move_value, -1, 1);
         right_move_value = Range.clip(right_move_value, -1, 1);
 
-        //lift_pos = Range.clip(lift_pos, -1, 1);
-        //lift_neg = Range.clip(lift_neg, -1, 0);
-
         claw_pos = (float) Range.clip(claw_pos, -1, 0.5);
-        //claw_neg = (float) Range.clip(claw_neg, -1, 0);
+        claw_neg = (float) Range.clip(claw_neg, -1, 0);
 
         //claw_neg = (float) Range.clip(claw_neg, 0,0.5 );
         //right_shooter = Range.clip(right_shooter, -1, 1);
@@ -137,107 +82,16 @@ public class SpringAssembly extends OpMode {
         left_move_value =  (float)scaleInput(left_move_value);
         right_move_value = (float)scaleInput(right_move_value);
 
-        //left_2 =  (float)scaleInput(left_2);
-        //right_2 = (float)scaleInput(right_2);
-
-        //lift_pos = (float) scaleInput(lift_pos);
-        //lift_neg = (float) scaleInput(lift_neg);
-
         claw_pos = (float) scaleInput(claw_pos);
-        //claw_neg = (float) scaleInput(claw_neg);
+        claw_neg = (float) scaleInput(claw_neg);
 
 
         // write the values to the motors
-      //  rightFront.setPower(-right_move_value/2);
-     //   leftFront.setPower(-left_move_value/2);
         rightMotor.setPower(right_move_value/2);
         leftMotor.setPower(left_move_value/2);
+        clawArm.setPower(claw_pos/1.5);
+        clawArm.setPower(claw_neg/1.5);
 
-        clawArm.setPower(-claw_pos/1.5);
-        //ClawArm.setPower(claw_neg);
-
-     /*   leftLift.setPower(lift_pos);
-        rightLift.setPower(lift_pos);
-        leftLift.setPower(lift_neg);
-        rightLift.setPower(lift_neg);
-*/
-
-        //if (gamepad1.right_bumper)
-        // {
-        // if the A button is pushed on gamepad1, increment the position of
-        // the arm servo.
-        //    ClawArm.setPower(1);
-        // }
-        //else if (!gamepad1.right_bumper)
-        // {
-        //    ClawArm.setPower(0);
-//
-//    }
-
-//    if (gamepad1.left_bumper)
-//    {
-        // if the Y button is pushed on5 gamepad1, decrease the position of
-        // the arm servo.
-//       ClawArm.setPower(-1);
-//    }
-//    else if (!gamepad1.left_bumper)
-//    {
-//       ClawArm.setPower(0);
-//    }
-
-        // update the position of the arm.
-        if (gamepad1.a)
-            // if the A button is pushed on gamepad1, increment the position of
-            // the gripper servo.
-            servoPosition -= speed;
-
-        if (gamepad1.y)
-            // if the B button is pushed on5 gamepad1, decrease the position of
-            // the gripper servo.
-            servoPosition += speed;
-
-
-    /*    servo.setPosition(servoPosition);
-        servoPosition  = Range.clip(servoPosition, 0, 1);
-
-        servo2.setPosition(servoPosition2);
-        servoPosition2  = Range.clip(servoPosition2, 0, 1);
-
-        servo3.setPosition(servoPosition3);
-        servoPosition3  = Range.clip(servoPosition3, 0, 1);
-        */
-        if (gamepad1.b)
-            // if the X button is pushed on gamepad1, increment the position of
-            // the griper servo.
-            servoPosition2 -= speed;
-
-        else if (gamepad1.x)
-            // if the Y button is pushed on5 gamepad1, decrease the position of
-            // the gripper servo.
-            servoPosition2 += speed;
-
-        if (gamepad2.a)
-            servoPosition3 += speed;
-        // clip the position values so that they never exceed their allowed range.
-        //armPosition = Range.clip(armPosition, ARM_MIN_RANGE, ARM_MAX_RANGE);
-        //ClawArm = Range.clip(CLAW_MIN_RANGE, CLAW_MAX_RANGE);
-
-        // write position values to the wrist and claw servo
-        //arm.setPosition(armPosition);
-        //claw.setPosition(clawPosition);
-
-      /*
-       * Send telemetry data back to driver station. Note that if we are using
-       * a legacy NXT-compatible motor controller, then the getPower() method
-       * will return a null value. The legacy NXT-compatible motor controllers
-       * are currently write only.
-       */
-
-      /*if ( button.isPressed() ) {
-
-
-
-        }*/
 
         telemetry.addData("Text", "*** Robot Data***");
         telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left_move_value));
